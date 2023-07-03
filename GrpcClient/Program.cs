5db8da6d43d1b99a7ae0bb46a8722df016c6fce9
keyvan <110492907+keyvan-arjmand@users.noreply.github.com>
2023-07-03 17:46:08 +0330

    Add project files.

---
+++ GrpcClient/Program.cs
@@ -0,0 +1,12 @@
+﻿
+using Grpc.Net.Client;
+using GrpcClient;
+
+Console.WriteLine("Perss your name : ");
+string name = Console.ReadLine();
+using var Channel = GrpcChannel.ForAddress("http://localhost:5242");
+var client = new Greeter.GreeterClient(Channel);
+var reply = await client.SayHelloAsync(new HelloRequest { Name = name });
+Console.WriteLine($"Greetings : {reply.Message}");
+
+Console.WriteLine("Perss Any Ket to Exist");
